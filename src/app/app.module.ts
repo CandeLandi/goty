4a470cc7275dc35227f066b8ce3627c0e113ec73
Candela Landi <landicandela01@gmail.com>
2024-10-11 14:51:49 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GotyComponent } from './pages/goty/goty.component';
+import { ComponentsModule } from "./components/components.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GotyComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
